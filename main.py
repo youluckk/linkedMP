a69d3b59b59bd0ae12981a38aa5c7b0baa13b8cb
youluckk <79311637+youluckk@users.noreply.github.com>
2023-08-29 12:52:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Fonction pour extraire les informations du produit à partir de l'URL
 def extract_product_info(url):
     try:
         response = requests.get(url)
         response.raise_for_status()
+
         soup = BeautifulSoup(response.text, 'html.parser')
-        product_name = soup.find('h1', class_='product-title').text.strip()
-        product_price = soup.find('span', class_='product-price').text.strip()
+
+        product_name_tag = soup.find('h1')
+        product_name = product_name_tag.text.strip() if product_name_tag else "Nom du produit non trouvé"
+
+        price_elements = soup.find_all('span', class_='woocommerce-Price-amount')
+        if len(price_elements) >= 2:
+            product_price = price_elements[1].find('bdi').text.strip()
+        else:
+            product_price = "Prix du produit non trouvé"
         return product_name, product_price
     except Exception as e:
         print("Une erreur s'est produite lors de l'extraction des informations du produit:", str(e))
         return None, None
 
-product_url = "https://www.example.com/product"
+product_url = "https://www.example.com/product/12345"
 
 product_name, product_price = extract_product_info(product_url)
 
